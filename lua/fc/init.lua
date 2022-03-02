3f34328f729c43bc60cdc93dad53d43e2cbdc8ef
SuperCuber <amit.gold00@gmail.com>
2022-03-02 14:39:19 +0200

    Implement initial caching

--- lua/fc/init.lua
+++ lua/fc/init.lua
@@ -3,21 +3,20 @@ local M = {};
 function M.setup()
 end
 
+-- TODO: unit testing, documentation
+
 function M._check_fc()
-    -- TODO: wait for vim to add better detection
+    -- TODO: wait for vim to add better detection, and support more shell+os combos
     if string.match(vim.fn.argv(0), "bash%-fc") then
-        M._shell = "bash"
-        -- fix for git-bash on windows
-        vim.api.nvim_set_option("shellcmdflag", "-c")
+        -- This is required in environments like git-bash on windows since &shell will be cmd/powershell
+        M._shell = { "bash", "-c" }
         M._setup_fc_environment()
     end
 end
 
 function M._setup_fc_environment()
     -- TODO: for testing, remove
-    if M._shell == nil then
-        M._shell = "bash"
-    end
+    M._shell = { "bash", "-c" }
 
     -- Main editing window
     M._editor_win = vim.api.nvim_get_current_win()
@@ -34,6 +33,8 @@ function M._setup_fc_environment()
 
     -- Editing loop
     vim.api.nvim_set_current_win(M._editor_win)
+    -- this is a list of {command, output} from the last run
+    M._last_command_state = {}
     vim.cmd ([[
         augroup FcNvimBuf
             au!
@@ -42,12 +43,46 @@ function M._setup_fc_environment()
     ]])
 end
 
+local function cleanup_command(command)
+    -- trim, remove trailing pipe
+   return string.gsub(vim.trim(command), "%s*|$", "")
+end
+
 function M._editor_buf_written()
-    local command = vim.api.nvim_buf_get_lines(M._editor_buf, 0, vim.api.nvim_buf_line_count(M._editor_buf), true)
-    local output = vim.fn.systemlist(M._shell, command)
+    local editor_contents = table.concat(vim.api.nvim_buf_get_lines(M._editor_buf, 0, -1, false), "\n")
+    -- TODO: this doesn't behave like shells since my stdout and stderr are combined
+    local command = vim.tbl_map(cleanup_command, vim.split(editor_contents, "|"))
+
+    -- last save had more lines than current command, cut them off
+    if #M._last_command_state > #command then
+        for i=#command+1,#M._last_command_state do
+            M._last_command_state[i] = nil
+        end
+    end
+
+    -- keep output from unchanged lines
+    -- by default we execute from past the commands (nothing changed so dont rerun)
+    local execute_from_command = #command + 1
+    for i, _ in ipairs(command) do
+        if M._last_command_state[i] == nil or M._last_command_state[i].command ~= command[i] then
+            execute_from_command = i
+            break
+        end
+    end
+
+    -- re-run changed lines
+    for i=execute_from_command,#command do
+        local shell_command = vim.deepcopy(M._shell)
+        table.insert(shell_command, command[i])
+        M._last_command_state[i] = {
+            command = command[i],
+            output = vim.fn.systemlist(shell_command, (M._last_command_state[i-1] or {}).output)
+        }
+        -- TODO: check for error status code and break
+    end
 
     vim.api.nvim_buf_set_option(M._output_buf, "modifiable", true)
-    vim.api.nvim_buf_set_lines(M._output_buf, 0, vim.api.nvim_buf_line_count(M._output_buf), true, output)
+    vim.api.nvim_buf_set_lines(M._output_buf, 0, vim.api.nvim_buf_line_count(M._output_buf), true, M._last_command_state[#M._last_command_state].output)
     vim.api.nvim_buf_set_option(M._output_buf, "modifiable", false)
 end
 
